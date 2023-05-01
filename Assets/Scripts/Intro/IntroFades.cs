17736ca90ccf87ea245697009088c3a41f87abc7
Andreas Wilcox <andreas@anwilc.com>
2023-05-02 01:05:54 +0200

    Added intro music fade. Increased font size on all descriptions, increased character info portrait size

--- Assets/Scripts/Intro/IntroFades.cs
+++ Assets/Scripts/Intro/IntroFades.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class IntroFades : MonoBehaviour
 {
     public FadeEffect FadeEffect;
+    public AudioSource Music;
 
     public void Start()
     {
@@ -19,7 +20,21 @@ public class IntroFades : MonoBehaviour
     public void FadeOut()
     {
         FadeEffect.FadeIn();
+        StartCoroutine(FadeOutMusic());
         StartCoroutine(WaitAndChangeToGameScene());
+    }
+
+    private IEnumerator FadeOutMusic()
+    {
+        var startVolume = Music.volume;
+        var startTime = Time.time;
+        while (Music.volume > 0)
+        {
+            var elapsed = (Time.time - startTime) / FadeEffect.fadeTime;
+            Music.volume = startVolume * (1 - elapsed);
+
+            yield return null;
+        }
     }
 
     private IEnumerator WaitAndChangeToGameScene()
